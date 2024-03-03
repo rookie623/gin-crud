4d6ea91f319f6425c357cd4651662f98ba9dc051
Matias Menoni <rookiemat@gmail.com>
2024-03-03 17:02:23 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rookie623/gin-crud/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// listen and serve on 0.0.0.0:8080
+	if err := router.Run(); err != nil {
+		return
+	}
+}
